9af926263df94ceeff236d879b89c9dc28009003
CherubayevS <CherubayevS@gmail.com>
2025-01-26 16:59:15 +0000

    Initial commit for dbt_clickhouse_lab

--- models/marts/f_orders_stats.sql
+++ models/marts/f_orders_stats.sql
@@ -1,8 +1,24 @@
 {{
     config (
-      engine='MergeTree()',
-      order_by=['']
+        engine='MergeTree()',
+        order_by=['O_ORDERYEAR']
     )
 }}
 
-SELECT 1
+SELECT
+    toYear(O_ORDERDATE) AS O_ORDERYEAR,
+    O_ORDERSTATUS,
+    O_ORDERPRIORITY,
+    count(DISTINCT O_ORDERKEY) AS num_orders,
+    count(DISTINCT C_CUSTKEY) AS num_customers,
+    SUM(L.L_EXTENDEDPRICE * L.L_DISCOUNT) AS revenue
+FROM
+    {{ ref('stg_orders') }} O
+    INNER JOIN {{ ref('stg_customer') }} C ON O.O_CUSTKEY = C.C_CUSTKEY
+    INNER JOIN {{ ref('stg_lineitem') }} L ON O.O_ORDERKEY = L.L_ORDERKEY
+WHERE
+    1 = 1
+GROUP BY
+    toYear(O_ORDERDATE),
+    O_ORDERSTATUS,
+    O_ORDERPRIORITY
